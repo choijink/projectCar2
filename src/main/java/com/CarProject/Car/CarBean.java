0e98a6720c2f0a968901dc70ba7b8a597fc909e3
YoungHyeonKim <dugusl@naver.com>
2024-10-23 17:00:38 +0900

    car bean 수정

--- src/main/java/com/CarProject/Car/CarBean.java
+++ src/main/java/com/CarProject/Car/CarBean.java
@@ -1,29 +1,38 @@
 package com.CarProject.Car;
 
 public class CarBean {
-//	 carmain
-	private int cIdx ; // 
-	private String domesticImport ; // 
-	private String brand ; // 
-	private String vehicleSize ; // 
-	private String carModel ; // 
-	private String carName ; // 
-	private String brandMark ; // 
-	private String carImage ; // 
+//	 car main
+	private int cIdx ; // 차량 식별 번
+	private String domesticImport ; // 국내/수
+	private String brand ; // 브랜
+	private String vehicleSize ; // 차량크기(대형, 중형, 등등...)
+	private String carModel ; // 차종(세단, 스포, 등등...)
+	private String carName ; // 모델명(k5, AMG A클래스, 등등...)
+	private String brandMark ; // 브랜드 로고 이미
+	private String carImage ; // 차량 이미
 	
 
 //	 cardetail1
 	private int cd1Idx ; //
-	private String FuelType ; //
-	private String FuelType ; //
-	private String FuelType ; //
-	private String FuelType ; //
-	private String FuelType ; //
-	private String FuelType ; //
-	private String FuelType ; //
-	private String FuelType ; //
-	private String FuelType ; //
-	private String FuelType ; //
-	private String FuelType ; //
+	private String fuelType ; // 유종(가솔린, 디젤, EV)
+	private String displacement ; // 배기량
+	private String fuelEfficiency ; // 유종(가솔린, 디젤, EV)
+	private String seatingCapacity ; // 인승
+	private String engineType ; // 엔진형식
+	private String driveType ; // 구동방식(사륜, 전륜, 후륜)
+	private String maxPower ; // 최고 출력
+	private String maxTorque ; // 최고 토크
+	private String length ; // 전장
+	private String width ; // 전폭
+	private String height ; // 전고
+	private String wheelBase ; // 축거
+	private String curbWeight ; // 공차중량
+	private String grade ; // 등급
+	
+	
+//	 cardetail1
+	private int cd2Idx ; //
+	private String trim ; // 트림
+	private String price ; // 가격
 
 }
