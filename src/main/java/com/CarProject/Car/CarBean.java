088f822c005609cf6522d332978fdbb521b12853
YoungHyeonKim <dugusl@naver.com>
2024-10-23 18:56:07 +0900

    carBean getter/setter 추가

--- src/main/java/com/CarProject/Car/CarBean.java
+++ src/main/java/com/CarProject/Car/CarBean.java
@@ -34,5 +34,168 @@ public class CarBean {
 	private int cd2Idx ; //
 	private String trim ; // 트림
 	private String price ; // 가격
+	
+	
+	
+	
+	
+	public int getcIdx() {
+		return cIdx;
+	}
+	public void setcIdx(int cIdx) {
+		this.cIdx = cIdx;
+	}
+	public String getDomesticImport() {
+		return domesticImport;
+	}
+	public void setDomesticImport(String domesticImport) {
+		this.domesticImport = domesticImport;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getVehicleSize() {
+		return vehicleSize;
+	}
+	public void setVehicleSize(String vehicleSize) {
+		this.vehicleSize = vehicleSize;
+	}
+	public String getCarModel() {
+		return carModel;
+	}
+	public void setCarModel(String carModel) {
+		this.carModel = carModel;
+	}
+	public String getCarName() {
+		return carName;
+	}
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+	public String getBrandMark() {
+		return brandMark;
+	}
+	public void setBrandMark(String brandMark) {
+		this.brandMark = brandMark;
+	}
+	public String getCarImage() {
+		return carImage;
+	}
+	public void setCarImage(String carImage) {
+		this.carImage = carImage;
+	}
+	public int getCd1Idx() {
+		return cd1Idx;
+	}
+	public void setCd1Idx(int cd1Idx) {
+		this.cd1Idx = cd1Idx;
+	}
+	public String getFuelType() {
+		return fuelType;
+	}
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+	public String getDisplacement() {
+		return displacement;
+	}
+	public void setDisplacement(String displacement) {
+		this.displacement = displacement;
+	}
+	public String getFuelEfficiency() {
+		return fuelEfficiency;
+	}
+	public void setFuelEfficiency(String fuelEfficiency) {
+		this.fuelEfficiency = fuelEfficiency;
+	}
+	public String getSeatingCapacity() {
+		return seatingCapacity;
+	}
+	public void setSeatingCapacity(String seatingCapacity) {
+		this.seatingCapacity = seatingCapacity;
+	}
+	public String getEngineType() {
+		return engineType;
+	}
+	public void setEngineType(String engineType) {
+		this.engineType = engineType;
+	}
+	public String getDriveType() {
+		return driveType;
+	}
+	public void setDriveType(String driveType) {
+		this.driveType = driveType;
+	}
+	public String getMaxPower() {
+		return maxPower;
+	}
+	public void setMaxPower(String maxPower) {
+		this.maxPower = maxPower;
+	}
+	public String getMaxTorque() {
+		return maxTorque;
+	}
+	public void setMaxTorque(String maxTorque) {
+		this.maxTorque = maxTorque;
+	}
+	public String getLength() {
+		return length;
+	}
+	public void setLength(String length) {
+		this.length = length;
+	}
+	public String getWidth() {
+		return width;
+	}
+	public void setWidth(String width) {
+		this.width = width;
+	}
+	public String getHeight() {
+		return height;
+	}
+	public void setHeight(String height) {
+		this.height = height;
+	}
+	public String getWheelBase() {
+		return wheelBase;
+	}
+	public void setWheelBase(String wheelBase) {
+		this.wheelBase = wheelBase;
+	}
+	public String getCurbWeight() {
+		return curbWeight;
+	}
+	public void setCurbWeight(String curbWeight) {
+		this.curbWeight = curbWeight;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public int getCd2Idx() {
+		return cd2Idx;
+	}
+	public void setCd2Idx(int cd2Idx) {
+		this.cd2Idx = cd2Idx;
+	}
+	public String getTrim() {
+		return trim;
+	}
+	public void setTrim(String trim) {
+		this.trim = trim;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
 
+	
+	
 }
