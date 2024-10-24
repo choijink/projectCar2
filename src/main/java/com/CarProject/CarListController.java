049ef57658fb3d7f1c88fad985f66403befe3681
YoungHyeonKim <dugusl@naver.com>
2024-10-24 22:16:46 +0900

    carList page 수정 중

---
+++ src/main/java/com/CarProject/CarListController.java
@@ -0,0 +1,19 @@
+package com.CarProject;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+	@WebServlet("/carList")  // /home URL과 매핑
+	public class CarListController extends HttpServlet {
+	    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	        // 비즈니스 로직 처리 (필요 시)
+	        
+	        // hello.jsp로 포워딩
+	        request.getRequestDispatcher("/WEB-INF/view/car/carList.jsp").forward(request, response);
+	    }
+}
